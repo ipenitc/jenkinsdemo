5ae637f89b75ba3d77b200dfb0ff7f03e0f87fb2
dipenitc <Dipendra.Hamal@informationtechconsultants.co.uk>
2026-04-17 15:10:30 +0100

    fix: update test case

--- src/test/java/com/dipen/jenkinsdemo/JenkinsDemoApplicationTests.java
+++ src/test/java/com/dipen/jenkinsdemo/JenkinsDemoApplicationTests.java
@@ -16,9 +16,9 @@ class JenkinsDemoApplicationTests {
         // A simple dummy test that always passes
         assertEquals(10, 5 + 5, "5 + 5 should equal 10");
     }
-    @Test
-    void testMathFailure() {
-        // This will fail and stop your Jenkins pipeline
-        assertEquals(11, 5 + 5, "This test is designed to fail!");
-    }
+//    @Test
+//    void testMathFailure() {
+//        // This will fail and stop your Jenkins pipeline
+//        assertEquals(11, 5 + 5, "This test is designed to fail!");
+//    }
 }
