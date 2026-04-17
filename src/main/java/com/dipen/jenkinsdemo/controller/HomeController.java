abf89b03db9d8e26a5515690d3889cee7498c52f
dipenitc <Dipendra.Hamal@informationtechconsultants.co.uk>
2026-04-17 09:52:21 +0100

    fix: update home controller

--- src/main/java/com/dipen/jenkinsdemo/controller/HomeController.java
+++ src/main/java/com/dipen/jenkinsdemo/controller/HomeController.java
@@ -9,6 +9,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HomeController {
     @GetMapping()
     public String welcome(){
-        return "Welcome to Jenkins Demo Application! from updated jenkinsfile";
+        return "Welcome to Jenkins Demo Application! from updated jenkinsfile. It is working as expected";
     }
 }
